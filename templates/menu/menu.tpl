be0bfd03902dad3cba8d55c4885e5afab1dc74c6
Unknown <pedroleao18@gmail.com>
2017-12-06 10:44:27 +0000

    Adicção do Menu

--- templates/menu/menu.tpl
+++ templates/menu/menu.tpl
@@ -1,7 +1,13 @@
 
 <section id="menu">
-  <header>
-    <h2>Ola Ola</h2>
-  </header>
 
+  <div class="menu">
+      <div class="text_menu" style="padding-top:0.5em;"> <p style="line-height: 0em;"> <img src="http://paginas.fe.up.pt/~ee12144/trabalhosSiem/trabalhoPHP-1/Fotos/kombi_menu.png" style="width:2em;height:1.2em;"> </p> <p> <a class= "simple" href = "http://paginas.fe.up.pt/~ee12144/trabalhosSiem/trabalhoPHP-1/index.php" target="_self">KombiWithUs  </a> </p> </div>
+      <div class="text_menu"> About US </div>
+      <div class="text_menu"> Regions </div>
+      <div class="text_menu"> Tours </div>
+      <div class="text_menu"> Founders </div>
+      <div class="text_menu"> Downloads </div>
+      <div class="SignIn"> SignIn </div>
+  </div>
 </section>
--- templates_c/b878a5261d657cbb1828efe556d9fecad85c8cac_0.file.menu.tpl.php
+++ templates_c/b878a5261d657cbb1828efe556d9fecad85c8cac_0.file.menu.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.30, created on 2017-11-29 10:28:19
+/* Smarty version 3.1.30, created on 2017-12-06 10:38:14
   from "/usr/users2/mieec2012/ee12144/public_html/trabalhosSiem/trabalhoPHP-2/templates/menu/menu.tpl" */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.30',
-  'unifunc' => 'content_5a1e8bc3ee6ef7_61766161',
+  'unifunc' => 'content_5a27c8963a1f50_90782585',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'b878a5261d657cbb1828efe556d9fecad85c8cac' => 
     array (
       0 => '/usr/users2/mieec2012/ee12144/public_html/trabalhosSiem/trabalhoPHP-2/templates/menu/menu.tpl',
-      1 => 1511951143,
+      1 => 1512556690,
       2 => 'file',
     ),
   ),
@@ -20,14 +20,20 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5a1e8bc3ee6ef7_61766161 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5a27c8963a1f50_90782585 (Smarty_Internal_Template $_smarty_tpl) {
 ?>
 
 <section id="menu">
-  <header>
-    <h2>Ola Ola</h2>
-  </header>
 
+  <div class="menu">
+      <div class="text_menu" style="padding-top:0.5em;"> <p style="line-height: 0em;"> <img src="http://paginas.fe.up.pt/~ee12144/trabalhosSiem/trabalhoPHP-1/Fotos/kombi_menu.png" style="width:2em;height:1.2em;"> </p> <p> <a class= "simple" href = "http://paginas.fe.up.pt/~ee12144/trabalhosSiem/trabalhoPHP-1/index.php" target="_self">KombiWithUs  </a> </p> </div>
+      <div class="text_menu"> About US </div>
+      <div class="text_menu"> Regions </div>
+      <div class="text_menu"> Tours </div>
+      <div class="text_menu"> Founders </div>
+      <div class="text_menu"> Downloads </div>
+      <div class="SignIn"> SignIn </div>
+  </div>
 </section>
 <?php }
 }
